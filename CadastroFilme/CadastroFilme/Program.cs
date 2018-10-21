73c02ae8bc222c775e7dc8fa98d7d5d4235cbbbc
“Renato” <renato.olimelo@gmail.com>
2018-10-21 14:09:18 -0300

    Implantado Menu

--- CadastroFilme/CadastroFilme/Program.cs
+++ CadastroFilme/CadastroFilme/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CadastroFilme {
     class Program {
         static void Main(string[] args) {
+
+            int opcao = 0;
+
+            while (opcao != 5) {
+
+                Console.Clear();
+                Tela.menu();
+
+                try {
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Erro inesperado: " + e.Message);
+                    opcao = 0;
+                }
+
+                switch (opcao) {
+
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Fim do programa!");
+                        break;
+
+                    default:
+                        Console.WriteLine("opcao inválida");
+                        break;
+                }
+                Console.ReadLine();
+            }
+
         }
     }
 }
---
+++ CadastroFilme/CadastroFilme/Tela.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CadastroFilme {
+    class Tela {
+
+        public static void menu() {
+            Console.WriteLine("-------------------Menu-------------------");
+            Console.WriteLine("1 – Listar artistas");
+            Console.WriteLine("2 – Cadastrar artista");
+            Console.WriteLine("3 – Cadastrar filme");
+            Console.WriteLine("4 – Mostrar dados de um filme");
+            Console.WriteLine("5 – Sair");
+            Console.WriteLine("");
+            Console.Write("Escolha uma opção: ");
+        }
+    }
+}
