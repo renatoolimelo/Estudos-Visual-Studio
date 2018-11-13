2016e3c4dd92e270c79464477d8d3d08b3cdf5f9
“Renato” <renato.olimelo@gmail.com>
2018-11-13 14:48:52 -0200

    Subindo Palindromo

---
+++ Palindromo/Palindromo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Palindromo {
+    class Program {
+        static void Main(string[] args) 
+            {
+
+            string word;
+
+            Console.WriteLine("Informe a palavra: ");
+            word = Console.ReadLine().ToLower();
+
+            char[] wordnormal = word.ToCharArray();
+            char[] palindrome = word.ToCharArray();
+
+            Array.Reverse(palindrome);
+
+            int j = wordnormal.Length - 1;
+
+            for (int i = 0; i < wordnormal.Length; i++) {
+                if (wordnormal[i] != palindrome[j]) {
+                    Console.WriteLine("Palavra não é um Palindromo");
+                    return;
+                }
+                else if(wordnormal[i] == palindrome[j] && j == 0) {
+                    Console.WriteLine("Palavra é um Palindromo");
+                }
+                j--;
+            }
+
+        }
+    }
+}
